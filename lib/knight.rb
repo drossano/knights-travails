d9f39f66b32d288a793b1316430c9028bc0e7839
Dean <72749793+drossano@users.noreply.github.com>
2022-03-15 19:13:33 -0400

    Perform breadth first search of all possible moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -9,15 +9,37 @@ class Knight
   end
 
   def get_moves(spaces)
-    # Iterate through each space
-    # Check if each move exists
-    # If move exists add to a nested array for each space
     spaces.each do |space|
       moves_from_space = [get_up_left(space), get_up_right(space), get_right_up(space), get_right_down(space), get_down_right(space), get_down_left(space), get_left_down(space), get_left_up(space)]
       @possible_moves.push(moves_from_space)
     end
     @possible_moves.map!{ |array| array.compact.sort}
-    p @possible_moves
+   @possible_moves
+  end
+
+  def search_moves(current_space)
+    queue = []
+    results = []
+    distance = 0
+    predecessor = nil
+    queue.push(current_space)
+    until queue.empty?
+      queue.each do |move|
+        unless results.any? { |result| result[:space] == move}
+          results.push(Hash[space: move, distance: distance, predecesor: predecessor])
+        end
+      end
+      predecessor = queue[0]
+      distance += 1
+      @possible_moves[@board.spaces.index(queue[0])].each do |move|
+        unless results.any? { |result| result[:space] == @board.spaces[move]}
+          queue.push(@board.spaces[move])
+        end
+      end
+      queue.shift
+    end
+    results.sort_by!{ |h| h[:distance] }
+    puts results
   end
 
   def get_up_left(space)
