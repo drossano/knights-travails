e4d9051fd8913789eb583b08d370e730abe2d20a
Dean <72749793+drossano@users.noreply.github.com>
2022-03-15 19:05:23 -0400

    Add repeated number spaces ie; 0,0 1,1... 7,7 to board

--- lib/board.rb
+++ lib/board.rb
@@ -2,8 +2,8 @@ class Board
   attr_reader :spaces
   def initialize
     @spaces = (0..7).to_a.permutation(2).to_a
-    @spaces.push([7,7])
-    @spaces.unshift([0,0])
+    @spaces.push([0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7])
+    @spaces.sort!
     p @spaces
   end
 end
